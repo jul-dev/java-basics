f905becc0cb6573ab383ede398789eecf962f058
julia <jkovrigin@gmail.com>
2019-04-25 22:20:01 -0400

    added ThinkJavaPractice

---
+++ src/LearningPackage/ThinkJavaParctice.java
@@ -0,0 +1,9 @@
+package LearningPackage;
+
+public class ThinkJavaParctice {
+    public static void main(String[] args) {
+        //Practicing Java
+        System.out.println("Hello World");
+        System.out.println("How are you doing?");
+    }
+}
