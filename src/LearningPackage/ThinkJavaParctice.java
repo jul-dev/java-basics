7b43b2b5099ba04f4a38b7e440858341c40d9167
julia <jkovrigin@gmail.com>
2019-04-30 21:05:18 -0400

    added book schlidt

--- src/LearningPackage/ThinkJavaParctice.java
+++ src/LearningPackage/ThinkJavaParctice.java
@@ -1,7 +1,7 @@
 package LearningPackage;
 
 public class ThinkJavaParctice {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         //Practicing Java
         System.out.println("Hello World");
         System.out.println("How are you doing?");
---
+++ src/book_schildt/App.java
@@ -0,0 +1,9 @@
+package book_schildt;
+import  book_schildt.mypkg.Balance;
+
+public class App {
+    public static void main(String[] args) {
+        Balance b = new Balance();
+        b.show();
+    }
+}
