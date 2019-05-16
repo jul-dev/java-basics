1a657d70ae095a6b761f1a16865be07dd25fc0a8
julia <jkovrigin@gmail.com>
2019-05-15 21:35:48 -0400

    New Class temp

---
+++ src/temp/TestProtected.java
@@ -0,0 +1,16 @@
+package temp;
+
+
+import LearningPackage.StudentAppDemo;
+
+public class TestProtected extends LearningPackage.Student {
+    /*TestProtected(){
+        super("f","Cali","Coll");
+*/
+    // }
+    public static void main(String[] args) {
+        StudentAppDemo o=new StudentAppDemo();
+        o.run();
+
+    }
+}
